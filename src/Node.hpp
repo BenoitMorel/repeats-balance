baa86cc46161205efe88c14e93800bbf65e4fbec
benoit <benomorel@gmail.com>
2016-10-20 15:30:05 +0200

    add a random tree generator

--- src/Node.hpp
+++ src/Node.hpp
@@ -2,13 +2,15 @@
 #define _RB_NODE_
 
 #include <iostream>
+#include <cstdlib>
 #include <vector>
+#include <algorithm>
 #include "repeatsbalance.h"
 
 
 class Node {
   public:
-    Node() : _left(0), _right(0), _seq(0), _max_identifier(0) {      
+    Node() : _left(0), _right(0), _seq_index(0), _max_identifier(0) {      
     }
 
     void set_children(Node *left, Node *right) {
@@ -16,25 +18,26 @@ class Node {
       _right = right;
     }
 
-    void set_sequence(const char *seq) {
-      _seq = seq;
+    void set_sequence(int seq_index) {
+      _seq_index = seq_index;
     }
     
     /*
-     * Fills the site repeats identifiers in this and update the global o_srcounts
+     * Fill the site repeats identifiers in this from the input sequences and update the global o_srcounts
      * Must be called in postprocess order
      */
-    void fill_identifier(int sitesNumber, std::vector<int> &o_srcounts) {
+    void fill_identifier(int sitesNumber, const InputSequences &sequences, std::vector<int> &o_srcounts) {
       _identifiers.resize(sitesNumber);
-      if (_seq) { // leaf node
+      if (!_left) { // leaf node
         // TODO dirty, must be optimized
         std::vector<int> map(256);
         std::fill(map.begin(), map.end(), 0);
         for (int site = 0; site < sitesNumber; site++) {
-          if (!map[_seq[site]]) {
-            map[_seq[site]] = ++_max_identifier;
+          char c = sequences._sequences[_seq_index][site];
+          if (!map[c]) {
+            map[c] = ++_max_identifier;
           }
-          _identifiers[site] = map[_seq[site]];
+          _identifiers[site] = map[c];
         }
 
       } else { // internal node
@@ -52,25 +55,57 @@ class Node {
         }
       }
     }
+
+    // not really random with respect to left and right. but it doesnt matter here
+    static Node *generate_random_tree(unsigned int leafs_number, unsigned int seed, std::vector<Node> &nodes) {
+      srand(seed);
+      nodes = std::vector<Node>(2 * leafs_number - 1);
+      std::vector<int> leaves(leafs_number);
+      for (unsigned int i = 0; i < leafs_number; ++i) {
+        leaves[i] = i;
+      }
+      std::random_shuffle(leaves.begin(), leaves.end());
+      for (unsigned int i = 0; i < leafs_number; ++i) {
+        nodes[i]._seq_index = leaves[i];
+      }
+      
+      //build the initial tree
+      //nodes[leafs_number] will always stay the root 
+      nodes[leafs_number]._left = &nodes[0];
+      nodes[leafs_number]._right = &nodes[1];
+
+      //stepwise addition
+      for (unsigned int i = 2; i < leafs_number; ++i) { 
+        int parent = (rand() % (i - 1)) + leafs_number;
+        Node *curr_inner = &nodes[i - 1 + leafs_number];
+        curr_inner->_left = &nodes[i];
+        if (rand() % 2) {
+          curr_inner->_right = nodes[parent]._left;
+          nodes[parent]._left = curr_inner;
+        } else { 
+          curr_inner->_right = nodes[parent]._right;
+          nodes[parent]._right = curr_inner;
+        }
+      } 
+      return &nodes[leafs_number];
+    }
     
     friend std::ostream& operator<< (std::ostream &out, const Node &node) {
-      out << "id : " << node.id << std::endl;
-      out << "identifiers : "; 
-      for (unsigned int i = 0 ; i < node._identifiers.size(); ++i) {
-        out << node._identifiers[i] << ' ';
+      if (node._left) {
+        out << "(" << *node._left << "," << *node._right << ")"; 
+      } else {
+        out << node._seq_index;
       }
-      out << std::endl;
       return out;
     }
 
-    int id;
     
 
   private:
     Node *_left;
     Node *_right;
-    const char *_seq;
-    int _max_identifier;
+    unsigned int _seq_index;
+    unsigned int _max_identifier;
     std::vector<int> _identifiers; // 1 to _max_identifier
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,26 @@
 
 #include <iostream>
 #include <vector>
+#include <time.h>
 #include "repeatsbalance.h"
 #include "Node.hpp"
 
-int check(std::vector<Node> &nodes, int seq_size, int *expectedSRNumbers, const char *test_name) {
+int check(std::vector<Node> &nodes, InputSequences & sequences, int *expectedSRNumbers, const char *test_name) {
+  unsigned int seq_size = sequences._seq_size;
   std::vector<int> srcounts(seq_size);
   for (unsigned int node = 0; node < nodes.size(); ++node) {
-    nodes[node].id = node;
-    nodes[node].fill_identifier(seq_size, srcounts);
+    nodes[node].fill_identifier(seq_size, sequences, srcounts);
   } 
   std::cout << "srcounts : " ;
   for (unsigned int i = 0 ; i < srcounts.size(); ++i) {
     if (srcounts[i] != expectedSRNumbers[i]) {
       std::cout << "ERROR IN " << test_name << std::endl;
       std::cout << "Expected: "; 
-      for (int j = 0; j < seq_size; ++j) {
+      for (unsigned int j = 0; j < seq_size; ++j) {
         std::cout << expectedSRNumbers[j] << ' ';
       }
       std::cout << std::endl << "Found:    ";
-      for (int j = 0; j < seq_size; ++j) {
+      for (unsigned int j = 0; j < seq_size; ++j) {
         std::cout << srcounts[j] << ' ';
       }
       std::cout << std::endl;
@@ -31,49 +32,44 @@ int check(std::vector<Node> &nodes, int seq_size, int *expectedSRNumbers, const 
 }
 
 int test1() {
-  const int seq_number = 4;
-  const int seq_size = 4;
+  InputSequences sequences;
+  parse_sequences("../data/simple_seq/simple4-4.phy", sequences);
   const int nodes_number = 7;
-  const char seq[seq_number][seq_size] = 
-                    {{'A','A','A','C'},     //   0---2------6 
-                     {'C','C','C','C'},     //   1---'      '
-                     {'G','G','G','G'},     //   3---5------'
-                     {'T','T','A','A'}};    //   4---'
-
   std::vector<Node> nodes(nodes_number);
   nodes[6].set_children(&nodes[2], &nodes[5]);
   nodes[2].set_children(&nodes[0], &nodes[1]);
   nodes[5].set_children(&nodes[3], &nodes[4]);
-  nodes[0].set_sequence(seq[0]);
-  nodes[1].set_sequence(seq[1]);
-  nodes[3].set_sequence(seq[2]);
-  nodes[4].set_sequence(seq[3]);
-  int expectedSRNumbers[seq_size] = {0, 3, 1, 1};
-  return check(nodes, seq_size, expectedSRNumbers, "test1");
+  nodes[0].set_sequence(0);
+  nodes[1].set_sequence(1);
+  nodes[3].set_sequence(2);
+  nodes[4].set_sequence(3);
+  int expectedSRNumbers[] = {0, 3, 1, 1};
+  return check(nodes, sequences, expectedSRNumbers, "test1");
 }
 
 int test2() {
-  const int seq_number = 5;
-  const int seq_size = 6;
+  InputSequences sequences;
+  parse_sequences("../data/simple_seq/simple5-6.phy", sequences);
   const int nodes_number = 9;
-  const char seq[seq_number][seq_size] = 
+  /*const char seq[seq_number][seq_size] = 
                     {{'C','A','A','A','C','C'},  // 0---2-------4-----8   
                      {'A','A','G','A','A','A'},  // 1---'       '     '
                      {'A','C','G','T','T','T'},  // 3-----------'     '
                      {'T','A','A','A','T','T'},  // 5---7--------------  
                      {'G','G','T','C','G','G'}}; // 6---'  
+                     */
   std::vector<Node> nodes(nodes_number);
   nodes[8].set_children(&nodes[4], &nodes[7]);
   nodes[4].set_children(&nodes[2], &nodes[3]);
   nodes[7].set_children(&nodes[5], &nodes[6]);
   nodes[2].set_children(&nodes[0], &nodes[1]);
-  nodes[0].set_sequence(seq[0]);
-  nodes[1].set_sequence(seq[1]);
-  nodes[3].set_sequence(seq[2]);
-  nodes[5].set_sequence(seq[3]);
-  nodes[6].set_sequence(seq[4]);
-  int expectedSRNumbers[seq_size] = {0, 0, 0, 1, 2, 4};
-  return check(nodes, seq_size, expectedSRNumbers, "test2");
+  nodes[0].set_sequence(0);
+  nodes[1].set_sequence(1);
+  nodes[3].set_sequence(2);
+  nodes[5].set_sequence(3);
+  nodes[6].set_sequence(4);
+  int expectedSRNumbers[] = {0, 0, 0, 1, 2, 4};
+  return check(nodes, sequences, expectedSRNumbers, "test2");
 }
 
 void test_sequences_parser (const std::string &sequences_file_name) {
@@ -94,11 +90,22 @@ void test_sequences_parser (const std::string &sequences_file_name) {
   }
 }
 
+void print_random(unsigned int leaves, unsigned int seed) {
+  std::vector<Node> nodes;
+  std::cout << *Node::generate_random_tree(leaves, seed, nodes) << std::endl;
+}
 
 int main()
 {
   test1();
   test2();
   test_sequences_parser("../data/minimal-6/minimal-6.phy");
+  std::cout << "random trees :" << std::endl;
+  print_random(10, time(0));
+  print_random(10, time(0) + 5);
+  print_random(10, time(0) + 10);
+  print_random(10, time(0) + 100);
+  print_random(10, time(0) + 190);
+  print_random(10, time(0) + 107);
   return 0;
 }
