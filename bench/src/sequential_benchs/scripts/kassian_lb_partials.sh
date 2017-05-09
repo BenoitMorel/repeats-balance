128afcfd5500699a5db411fdc9962aa9cb7251a8
benoit <benomorel@gmail.com>
2017-05-09 15:03:24 +0200

    add an option in the bencher to avoid updating repeats

--- bench/src/sequential_benchs/scripts/kassian_lb_partials.sh
+++ bench/src/sequential_benchs/scripts/kassian_lb_partials.sh
@@ -2,8 +2,8 @@
 
 source scripts/common.sh
 
-data_name=59
+data_name=kyte
 
- ./main kassian_lb_partials $path_data/$data_name/unrooted.newick $path_data/$data_name/$data_name.phy $path_data/$data_name/$data_name.part 4 1 1 0 5000 8 
+ ./main kassian_lb_partials $path_data/$data_name/unrooted.newick $path_data/$data_name/$data_name.phy $path_data/$data_name/$data_name.part 4 1 1 0 50 8 
 
 
