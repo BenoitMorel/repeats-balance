4432f1d0e56d95b7b8df7b47fd2a1679b3ac8386
Benoit Morel <morelbt@haswell-login.cluster.intern>
2017-06-27 10:34:04 +0200

    Improve the tool to analyse sites

--- src/tools/sites_histogram.cpp
+++ src/tools/sites_histogram.cpp
@@ -1,53 +1,86 @@
 #include "tools.hpp"
 #include <iostream>
+#include <fstream>
 #include <stdlib.h>
 #include "../common/repeatsbalance.hpp"
 #include <time.h>
 #include <math.h>
 
 typedef std::vector< std::vector<unsigned int> > Histogram;
+const unsigned int alphabet_size = 16;
+
 
 struct ProcessedPartition {
+  ProcessedPartition(MSA *msa): msa(msa), valid_sites(msa->get_length(), 1) {
+    histogram.resize(msa->get_length());
+    std::fill(histogram.begin(),
+      histogram.end(),
+      std::vector<unsigned int>(alphabet_size , 0));
+  }
+
+  ~ProcessedPartition() {
+    delete msa;
+  }
+
+
+  void compute_histogram()
+  {
+    char **seq = msa->get_pll_msa()->sequence;
+    for (unsigned int i = 0; i < msa->get_tips_number(); ++i) {
+      for (unsigned int j = 0; j < sites_number(); ++j) {
+       histogram[j][pll_map_nt[(unsigned int)seq[i][j]]]++;
+      }
+    }
+  }
+
+  void  display_histogram()
+  {
+    for (unsigned int i = 0; i < sites_number(); ++i) {
+      for (unsigned int j = 0; j < histogram[i].size(); ++j) {
+        std::cout << histogram[i][j] << ", ";
+      }
+      std::cout << std::endl;
+    }
+  }
+
+  void invalidate_uninformative_sites(unsigned int lim) 
+  {
+    for (unsigned int i = 0; i < sites_number(); ++i) {
+      unsigned int count_char = 0;
+      // skip gaps (last column in histogram)
+      for (unsigned int j = 0; j < histogram[i].size() - 1; ++j) {
+        count_char += (histogram[i][j] > lim);
+      }
+      valid_sites[i] &= (count_char > 1);
+    }
+  }
+
+  unsigned int sites_number() {
+    return msa->get_length();
+  }
+
+  void write_valid_seq(unsigned int taxon, std::string &buffer) {
+    for (unsigned int i = 0; i < sites_number(); ++i) {
+      if (valid_sites[i]) {
+        buffer.push_back(msa->get_pll_msa()->sequence[taxon][i]);
+      }
+    }
+  }
+  
+  unsigned int compute_valid_sites_number() {
+    unsigned int res = 0;
+    for (unsigned int i = 0; i < valid_sites.size(); ++i) {
+      res += valid_sites[i];
+    }
+    return res;
+  }
+
   MSA *msa;
   Histogram histogram;
   std::vector<char> valid_sites;
 };
 
-void compute_histogram(ProcessedPartition &partition)
-{
-  Histogram &histogram = partition.histogram;
-  MSA &msa = *partition.msa;
-  histogram.resize(msa.get_length());
-  std::fill(histogram.begin(),
-      histogram.end(),
-      std::vector<unsigned int>(msa.get_states_number() + 1, 0));
-  char **seq = msa.get_pll_msa()->sequence;
-  for (unsigned int i = 0; i < msa.get_tips_number(); ++i) {
-    for (unsigned int j = 0; j < msa.get_length(); ++j) {
-      if (seq[i][j] == '-') {
-        histogram[j][0]++;
-      } else if (seq[i][j] == 'A') {
-        histogram[j][1]++;
-      } else if (seq[i][j] == 'C') {
-        histogram[j][2]++;
-      } else if (seq[i][j] == 'G') {
-        histogram[j][3]++;
-      } else if (seq[i][j] == 'T') {
-        histogram[j][4]++;
-      }
-    }
-  }
-}
 
-void  display_histogram(Histogram &histogram)
-{
-  for (unsigned int i = 0; i < histogram.size(); ++i) {
-    for (unsigned int j = 0; j < histogram[i].size(); ++j) {
-      std::cout << histogram[i][j] << ", ";
-    }
-    std::cout << std::endl;
-  }
-}
 
 unsigned int count_unichar(Histogram &histogram, unsigned int lim) {
   unsigned int count = 0;
@@ -63,36 +96,60 @@ unsigned int count_unichar(Histogram &histogram, unsigned int lim) {
 
 void sites_histogram(int argc, char *params[])
 {
-  if (argc != 2) {
+  /////////////////////////
+  //  PARAMS
+  ///////////////////////
+  if (argc != 4) {
     std::cerr << "Error : syntax is" << std::endl;
     std::cerr 
-      << "phylip_file partition_file" 
+      << "phylip_file partition_file output limit" 
       << std::endl;
     return ;
   }
+  
   unsigned int i = 0;
   const char *phy_filename = params[i++];
   const char *part_filename = params[i++];
+  std::string output(params[i++]);
+  unsigned int lim = atoi(params[i++]);
   unsigned int states_number = 4;
+
+  ////////////////////////////
+  //  GOGO
+  ///////////////////////////
   MSA full_msa(phy_filename, states_number);
   std::vector<PartitionIntervals> initial_partitionning;
   PartitionIntervals::parse(part_filename, initial_partitionning);
   unsigned int partitions_number = initial_partitionning.size();
-  std::vector<ProcessedPartition> partitions(partitions_number);
+  std::vector<ProcessedPartition *> partitions;
   for (unsigned int i = 0; i < partitions_number; ++i) {
-    partitions[i].msa = new MSA(&full_msa, initial_partitionning[i], i);
+    partitions.push_back(new ProcessedPartition(new MSA(&full_msa, initial_partitionning[i], i)));
   }
   
-  
-  // histograms[i][0] = gaps
-  // histograms[i][1] = A
-  // histograms[i][2] = C
-  // histograms[i][3] = G
-  // histograms[i][4] = T 
   for (unsigned int i = 0; i < partitions.size(); ++i) { 
-    compute_histogram(partitions[i]);
-    display_histogram(partitions[i].histogram);
+    partitions[i]->compute_histogram();
+    partitions[i]->invalidate_uninformative_sites(lim);
+  }
+ 
+  std::ofstream os_phy((output + ".phy").c_str());
+  std::ofstream os_part((output + ".part").c_str());
+  unsigned int valid_sites_number = 0;
+  for (unsigned int p = 0; p < partitions.size(); ++p) {
+    if (p) {
+      os_part << std::endl;
+    }
+    os_part << "DNA, PARTITION_" << p << " = " << valid_sites_number + 1;
+    valid_sites_number += partitions[p]->compute_valid_sites_number();
+    os_part << "-" << valid_sites_number;
   }
   
+  os_phy << full_msa.get_tips_number() << " " << valid_sites_number << std::endl;
+  for (unsigned int t = 0; t < full_msa.get_tips_number(); ++t) {
+    std::string buffer;
+    for (unsigned int p = 0; p < partitions.size(); ++p) {
+      partitions[p]->write_valid_seq(t, buffer);
+    }
+    os_phy << full_msa.get_pll_msa()->label[t] << " " << buffer << std::endl;
+  }
 }
 
